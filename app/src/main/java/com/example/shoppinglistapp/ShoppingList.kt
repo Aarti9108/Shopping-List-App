2cff45e79471dee0c7a961d27cde9ec8fd71b025
Aarti <a.art.i.singh9108@gmail.com>
2025-06-28 20:38:12 +0530

    Added an alert dialog box

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,16 +26,17 @@ data class ShoppingItem(val id: Int,
                         var quantity:Int,
                         var isEditing: Boolean = false)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
 
     //Shopping list state that updates the UI when items change
-    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
-
+    val sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
     Column (modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ){
-        Button(onClick = {   },
+        Button(onClick = { showDialog = true },
             modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         ){
             Text("Add Items to the list")
@@ -48,5 +51,15 @@ fun ShoppingListApp(){
             }
 
         }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            {
+                Text(text = "Alert")
+
+            }
+        )
     }
 }
