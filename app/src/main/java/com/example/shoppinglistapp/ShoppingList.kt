cc5182a5de275bb5ad2341a1383a1e3498cf7706
Aarti <a.art.i.singh9108@gmail.com>
2025-07-02 00:29:48 +0530

    Created a funtion for edit button

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,11 +1,13 @@
 package com.example.shoppinglistapp
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -116,6 +118,52 @@ fun ShoppingListApp() {
         )
     }
 }
+
+@Composable
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit){
+    var editedName by remember { mutableStateOf(item.name) }
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row(modifier = Modifier.fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+    ){
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+            )
+
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = {editedQuantity = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+            )
+        }
+
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName,editedQuantity.toIntOrNull()?:1)
+        }) {
+            Text("Save")
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
 
 @Composable
 fun ShoppingListItem(
